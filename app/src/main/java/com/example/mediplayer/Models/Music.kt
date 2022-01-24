29111a1e1a19457c1a5ffbfd7c0102907b92ade1
Invoker-RPG <darxonbek@bk.ru>
2022-01-24 17:44:14 +0500

    Classes permissions

---
+++ app/src/main/java/com/example/mediplayer/Models/Music.kt
@@ -0,0 +1,5 @@
+package com.muhammadziyo.mediaplayer.Models
+
+import java.io.Serializable
+
+data class Music(val id:Long, val music:String, val imagePath:String, val musicPath:String, val artist:String):Serializable
